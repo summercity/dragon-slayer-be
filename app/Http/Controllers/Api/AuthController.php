ead21d8b78e89993b77fdfa2d8befffe9b00b871
summercity <jandave.arce.steam@gmail.com>
2019-07-25 16:13:12 +0800

    CORS

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
 
-        return response(['user'=> auth()->user(), 'access_token'=> $accessToken]);
+        return response(['user'=> auth()->user(), 'token'=> $accessToken, 'authenticated' => true]);
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,10 @@
 <?php
+// header('Access-Control-Allow-Origin: *');
+// header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 use Illuminate\Http\Request;
 
@@ -12,6 +18,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
